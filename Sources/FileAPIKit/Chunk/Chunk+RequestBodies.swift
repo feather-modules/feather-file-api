24bf9f720451f02798f8074ba2d1b176ade610db
mzpx <atomantinator@gmail.com>
2024-05-10 18:15:12 +0200

    limit fix (#2)

--- Sources/FileAPIKit/Chunk/Chunk+RequestBodies.swift
+++ Sources/FileAPIKit/Chunk/Chunk+RequestBodies.swift
@@ -7,7 +7,10 @@ extension File.Chunk {
 
         enum Upload: BinaryBody {
             static let contentType: OpenAPI.ContentType = .any
-            static let description = "Upload request body"
+            static let description = """
+                    Upload request body
+                    5 MiB to 5 GiB. There is no minimum size limit on the last part of your multipart upload.
+                """
         }
     }
 }
